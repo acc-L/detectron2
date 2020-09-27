ae12452dbb98649f3bae04e198fb8e39a9c8cd65
lishengyang@bilibili.com <lishengyang@bilibili.com>
2020-09-27 17:34:41 +0800

    debug

--- projects/PointRend_track/point_rend/datasets/coco.py
+++ projects/PointRend_track/point_rend/datasets/coco.py
@@ -21,19 +21,9 @@ class COCODataset(CustomDataset):
     def __init__(self,
                  ann_file,
                  img_prefix,
-                 img_scale,
-                 img_norm_cfg,
-                 size_divisor=None,
                  proposal_file=None,
                  num_max_proposals=1000,
-                 flip_ratio=0,
-                 with_mask=True,
-                 with_crowd=True,
-                 with_label=True,
                  with_track=False,
-                 extra_aug=None,
-                 aug_ref_bbox_param=None,
-                 resize_keep_ratio=True,
                  test_mode=False
                  ):
         # prefix of images path
@@ -48,50 +38,11 @@ class COCODataset(CustomDataset):
             self.proposals = None
         # filter images with no annotation during training
 
-        # (long_edge, short_edge) or [(long1, short1), (long2, short2), ...]
-        self.img_scales = img_scale if isinstance(img_scale,
-                                                  list) else [img_scale]
-        # normalization configs
-        self.img_norm_cfg = img_norm_cfg
-
-        # max proposals per image
-        self.num_max_proposals = num_max_proposals
-        # flip ratio
-        self.flip_ratio = flip_ratio
-        assert flip_ratio >= 0 and flip_ratio <= 1
-        # padding border to ensure the image size can be divided by
-        # size_divisor (used for FPN)
-        self.size_divisor = size_divisor
-        self.img_scales = img_scale if isinstance(img_scale,list) else [img_scale]
-
-        # with mask or not (reserved field, takes no effect)
-        self.with_mask = with_mask
-        # some datasets provide bbox annotations as ignore/crowd/difficult,
-        # if `with_crowd` is True, then these info is returned.
-        self.with_crowd = with_crowd
-        # with label is False for RPN
-        self.with_label = with_label
         self.with_track = with_track
-        # params for augmenting bbox in the reference frame
-        self.aug_ref_bbox_param = aug_ref_bbox_param
+        
         # in test mode or not
         self.test_mode = test_mode
 
-        # transforms
-        self.img_transform = ImageTransform(
-            size_divisor=self.size_divisor, **self.img_norm_cfg)
-        self.bbox_transform = BboxTransform()
-        self.mask_transform = MaskTransform()
-        self.numpy2tensor = Numpy2Tensor()
-
-        # if use extra augmentation
-        if extra_aug is not None:
-            self.extra_aug = ExtraAugmentation(**extra_aug)
-        else:
-            self.extra_aug = None
-
-        # image rescale if keep ratio
-        self.resize_keep_ratio = resize_keep_ratio
 
     def __len__(self):
         return len(self.vid_infos)
@@ -126,29 +77,6 @@ class COCODataset(CustomDataset):
         #return self._parse_ann_info(ann_info, frame_id)
         return ann_info
 
-    def bbox_aug(self, bbox, img_size):
-        assert self.aug_ref_bbox_param is not None
-        center_off = self.aug_ref_bbox_param[0]
-        size_perturb = self.aug_ref_bbox_param[1]
-        
-        n_bb = bbox.shape[0]
-        # bbox center offset
-        center_offs = (2*np.random.rand(n_bb, 2) - 1) * center_off
-        # bbox resize ratios
-        resize_ratios = (2*np.random.rand(n_bb, 2) - 1) * size_perturb + 1
-        # bbox: x1, y1, x2, y2
-        centers = (bbox[:,:2]+ bbox[:,2:])/2.
-        sizes = bbox[:,2:] - bbox[:,:2]
-        new_centers = centers + center_offs * sizes
-        new_sizes = sizes * resize_ratios
-        new_x1y1 = new_centers - new_sizes/2.
-        new_x2y2 = new_centers + new_sizes/2.
-        c_min = [0,0]
-        c_max = [img_size[1], img_size[0]]
-        new_x1y1 = np.clip(new_x1y1, c_min, c_max)
-        new_x2y2 = np.clip(new_x2y2, c_min, c_max)
-        bbox = np.hstack((new_x1y1,new_x2y2)).astype(np.float32)
-        return bbox
 
     def prepare_train_img(self, idx):
         # prepare a pair of image in a sequence
@@ -157,8 +85,6 @@ class COCODataset(CustomDataset):
         img = cv2.imread(osp.join(self.img_prefix, vid_info['file_name']))
         img_shape = img.shape[:2]
         #img = cv2.resize(img, self.img_scales[0])
-        basename = osp.basename(vid_info['file_name'])
-
         # load proposals if necessary
         if self.proposals is not None:
             proposals = self.proposals[idx][:self.num_max_proposals]
@@ -188,9 +114,6 @@ class COCODataset(CustomDataset):
         #gt_masks = [cv2.resize(self.ytvos.annToMask(ann), self.img_scales[0]) for ann in anns]
         gt_masks = [self.ytvos.annToMask(ann) for ann in anns]
 
-        img, _, pad_shape, scale_factor = self.img_transform(
-            img, self.img_scales[0], False, keep_ratio=self.resize_keep_ratio)
-        #img = img.copy()
         #gt_bboxes = self.bbox_transform(gt_bboxes, img_shape, scale_factor,
         #                               False)
 
@@ -203,14 +126,6 @@ class COCODataset(CustomDataset):
             return None
 
         ori_shape = (vid_info['height'], vid_info['width'], 3)
-        '''
-        img_meta = dict(
-            ori_shape=ori_shape,
-            img_shape=img_shape,
-            #pad_shape=pad_shape,
-            scale_factor=scale_factor,
-            flip=flip)
-        '''
 
         data = dict(
             image=to_tensor(img),
